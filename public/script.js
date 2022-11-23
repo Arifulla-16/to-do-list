7cfe66e45979cab1131fb9af128fe189ae75b0e0
Arifulla-16 <skarfistark@gmail.com>
2022-11-23 19:58:20 +0530

    css js

---
+++ public/script.js
@@ -0,0 +1,13 @@
+
+$("#text").focus(function (e) { 
+    $("#text").attr("placeholder","");
+    $("#text").css("border-bottom", "2px");
+    $("#text").css("border-style", "solid");
+    // $("#text").css("transition","border-bottom 0.2s ease-in");
+});
+
+$("#text").focusout(function (e) { 
+    $("#text").attr("placeholder","New Item");
+    $("#text").css("border-bottom", "0px");
+    // $("#text").css("transition","border-bottom 0.2s ease-in");
+});
